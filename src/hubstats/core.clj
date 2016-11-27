e12772d78fd22fad955057025a6028d14de29b70
Nicolas Kosinski <nicokosi@yahoo.com>
2016-11-27 18:26:43 +0100

    Fix issue #1: Wrong number of open/closed pull requests
    Remove number of open/closed pull request that were incorrect.
    
    Fixing counters would imply:
    1. traversing all pull requests though pagination.
    cf. https://developer.github.com/guides/traversing-with-pagination/
    2. increasing total duration (+30 seconds on my laptop)
    
    As a consequence, remove global counters (open/closed)
    and focus on data for last week.

--- src/hubstats/core.clj
+++ src/hubstats/core.clj
@@ -5,28 +5,7 @@
     [clj-time.format :as time-format])
   (:import (java.io IOException)))
 
-(defn pulls [org repo token]
-  (json/read-str
-    (slurp
-      (str "https://api.github.com/repos/" org "/" repo "/pulls?state=all&access_token=" token))))
-
 (def date-format (time-format/formatter "yyyy-MM-dd'T'HH:mm:ssZ"))
-
-(defn this-week? [x key]
-  (time/after?
-    (time-format/parse date-format (get x key))
-    (time/ago (time/weeks 1))))
-
-(defn updated-this-week? [x]
-  (this-week? x "updated_at"))
-
-(defn created-this-week? [x]
-  (this-week? x "created_at"))
-
-(defn open? [pr]
-  (= "open" (get pr "state")))
-(defn closed? [pr]
-  (= "closed" (get pr "state")))
 
 (defn events
   ([org repo token page]
@@ -44,8 +23,18 @@
   ([org repo token]
    (events org repo token 1 [])))
 
+(defn this-week? [map key]
+  (time/after?
+    (time-format/parse date-format (get map key))
+    (time/ago (time/weeks 1))))
+
+(defn created-this-week? [event]
+  (this-week? event "created_at"))
+
 (defn action [event] (get-in event ["payload" "action"]))
+
 (defn pr-closed? [event] (= (action event) "closed"))
+
 (defn pr-opened? [event] (= (action event) "opened"))
 
 (defn pr-review-comment-evt? [event] (= "PullRequestReviewCommentEvent" (get event "type")))
@@ -58,21 +47,15 @@
      repo (second args)
      token (nth args 2 "")
 
-     raw-prs (pulls org repo token)
-     open-prs (filter open? raw-prs)
-     closed-prs (filter closed? raw-prs)
-     pr-updated-this-week (filter updated-this-week? raw-prs)
-
      raw-events (events org repo token)
      raw-events-this-week (filter created-this-week? raw-events)
+
      pr-opened-this-week (filter pr-opened? raw-events-this-week)
      pr-closed-this-week (filter pr-closed? raw-events-this-week)]
 
     (println (str org "/" repo " pull requests:"))
 
-    (println (str (count open-prs) " open, " (count closed-prs)) "closed")
-
-    (println (str "last week: " (count pr-opened-this-week) " opened, " (count pr-updated-this-week) " updated, " (count pr-closed-this-week) " closed"))
+    (println (str "last week: " (count pr-opened-this-week) " opened, " (count raw-events-this-week) " updated, " (count pr-closed-this-week) " closed"))
 
     (println (str "PR opened per author for last week: "
                   (reverse
