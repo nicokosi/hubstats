758064799e2cc8bed752fa23e00065f8fc1adc93
Nicolas Kosinski <nicokosi@yahoo.com>
2018-01-22 07:06:10 +0100

    Upgrade Clojure to 1.9.0 and core.async lib to 0.4.47

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {
             :name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"],
+  :dependencies [[org.clojure/clojure "1.9.0"],
                  [org.clojure/data.json "0.2.6"],
                  [clj-http "2.3.0"],
                  [slingshot "0.12.2"],
                  [clj-time "0.12.2"],
-                 [org.clojure/core.async "0.2.395"]]
+                 [org.clojure/core.async "0.4.474"]]
   :main ^:skip-aot hubstats.core
   :profiles {:uberjar {:aot :all}})
