9489e38dec0818ae2197eb4ef5f24db32f303b08
Nicolas Kosinski <nicokosi@yahoo.com>
2018-01-22 07:06:10 +0100

    Upgrade libraries clj-http to 3.7.0 and clj-time to 0.14.2

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"],
                  [org.clojure/data.json "0.2.6"],
-                 [clj-http "2.3.0"],
+                 [clj-http "3.7.0"],
                  [slingshot "0.12.2"],
-                 [clj-time "0.12.2"],
+                 [clj-time "0.14.2"],
                  [org.clojure/core.async "0.4.474"]]
   :main ^:skip-aot hubstats.core
   :profiles {:uberjar {:aot :all}})
