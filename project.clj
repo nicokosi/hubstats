799a7d584fa569e48bfae03bdd3830db7b4136aa
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-12-28 13:39:44 +0100

    Upgrade to Clojure 1.10.0
    Clojure 1.10.0 is available sice Dec 17, 2018:
    https://clojure.org/community/downloads#_stable_release_1_10_0_dec_17_2018
    Grab it while it's hot! ♨

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {
             :name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"],
+  :dependencies [[org.clojure/clojure "1.10.0"],
                  [org.clojure/data.json "0.2.6"],
                  [clj-http "3.7.0"],
                  [slingshot "0.12.2"],
