2fbf5ff567b615422b58a1c6149d1f8f8d7da11e
Nicolas Kosinski <nicokosi@yahoo.com>
2020-06-08 21:23:20 +0200

    ⬆️ Upgrade dependencies to latest

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"],
-                 [org.clojure/data.json "0.2.6"],
-                 [clj-http "2.3.0"],
+                 [org.clojure/data.json "1.0.0"],
+                 [clj-http "3.10.1"],
                  [slingshot "0.12.2"],
                  [clj-time "0.15.2"],
-                 [org.clojure/core.async "0.4.500"]]
+                 [org.clojure/core.async "1.2.603"]]
   :main ^:skip-aot hubstats.core
   :plugins [[lein-ancient "0.6.15"]
             [lein-cljfmt "0.6.7"]]
