nicokosi/hubstats

commit 2edbca75a2675c4ba4764be5e32982f184eded6e

Fix #48: Use hato HTTP client and fix GitHub API authorization deprecation
Set the token in the HTTP header:
https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param

Change the HTTP client library. Use 'hato', a Java 11 HTTP client wrapper:
https://github.com/gnarroway/hato

src/hubstats/github.clj
@@ -3,32 +3,31 @@
    [clojure.data.json :as json]
    [clj-time.core :as time]
    [clj-time.format :as time-format]
-   [clj-http.client :as http-client]
+   [hato.client :as http-client]
    [slingshot.slingshot :refer [try+]]))
 (declare catch) (comment "catch is generated by try+ macro")
 
 (def date-format (time-format/formatter "yyyy-MM-dd'T'HH:mm:ssZ"))
 
-(defn github-api-events [org repo user-and-token page]
+(defn github-api-events [org repo token page]
   (let [url (str "https://api.github.com/repos/" org "/" repo "/events?page=" page)]
     (json/read-str
-     ((http-client/get url {:basic-auth user-and-token
-                            "Accept" "application/vnd.github.v3+json"})
-      :body))))
+     ((http-client/get url {:headers {"Authorization" (str "token " token)
+                                      "Accept" "application/vnd.github.v3+json"}}) :body))))
 
 (defn events
-  ([org repo user-and-token page]
+  ([org repo token page]
    (when (< page 100)
      (try+
-      (github-api-events org repo user-and-token page)
+      (github-api-events org repo token page)
       (catch [:status 422] {} nil))))
-  ([org repo user-and-token page acc]
-   (let [events (events org repo user-and-token page)]
+  ([org repo token page acc]
+   (let [events (events org repo token page)]
      (if (nil? events)
        acc
-       (recur org repo user-and-token (inc page) (concat acc events)))))
-  ([org repo user-and-token]
-   (events org repo user-and-token 1 [])))
+       (recur org repo token (inc page) (concat acc events)))))
+  ([org repo token]
+   (events org repo token 1 [])))
 
 (defn- since? [map key date]
   (time/after?
@@ -64,14 +63,14 @@
 
 (defn pr-stats [opts repo]
   (let [org (opts :org)
-        user-and-token (opts :token)
+        token (opts :token)
         since-date (get opts :since nil)
         days (Integer/parseInt (get opts :days "0"))
         weeks (Integer/parseInt (get opts :weeks "1"))
         date (if since-date
                (time-format/parse date-format since-date)
                (time/ago (if (> days 0) (time/days days) (time/weeks weeks))))
-        new-raw-events (filter #(created-since? % date) (events org repo user-and-token))
+        new-raw-events (filter #(created-since? % date) (events org repo token))
         pr-events (filter pr-event? new-raw-events)
         pr-review-comment-events (filter pr-review-comment-evt? new-raw-events)]
     (assoc {}

project.clj
@@ -6,7 +6,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.2"],
                  [org.clojure/data.json "1.0.0"],
-                 [clj-http "3.11.0"],
+                 [hato "0.8.0"],
                  [slingshot "0.12.2"],
                  [clj-time "0.15.2"],
                  [org.clojure/core.async "1.3.610"]]

commit cbd0f4941e29ab89d0c14fae6b5ad39585692704

Fix #53: Can query public repo without any token
Only set 'Authorisation' headers if token CLI option is present.
Fixes a regression introduced by commit 2edbca7.

src/hubstats/core.clj
@@ -15,8 +15,8 @@
     \t-o\t\t\tGitHub organization (shorthand)
     \t--repository\t\tGitHub repository
     \t-r\t\t\tGitHub repository (shorthand)
-    \t--token\t\t\tGitHub access token prefixed with GitHub user and ':', i.e 'myUser:myToken' (optional)
-    \t-t\t\t\tGitHub access token prefixed with GitHub user and ':', i.e 'myUser:myToken' (shorthand, optional)
+    \t--token\t\t\tGitHub access (optional)
+    \t-t\t\t\tGitHub access token (shorthand, optional)
     Optional parameters:
     \t--repositories\t\tComma-separated list of repositories (optional)
     \t--since-weeks\t\toutput events that occurred since this number of weeks (optional, default: 1)
@@ -27,7 +27,7 @@
     \t-s\t\t\toutput events that occurred since a date with format 'yyyy-MM-dd'T'HH:mm:ssZ' (shorthand, optional)
 
     Examples:
-    \tlein run --organization docker --repository containerd --token \"$user:$token\"
+    \tlein run --organization docker --repository containerd --token \"$token\"
     \tlein run --organization docker --repository containerd --since 2017-01-17T00:00:00Z
     \tlein run --organization docker --repository containerd --since-days 10
     \tlein run --organization docker --repositories docker,containerd"))

src/hubstats/github.clj
@@ -10,10 +10,9 @@
 (def date-format (time-format/formatter "yyyy-MM-dd'T'HH:mm:ssZ"))
 
 (defn github-api-events [org repo token page]
-  (let [url (str "https://api.github.com/repos/" org "/" repo "/events?page=" page)]
-    (json/read-str
-     ((http-client/get url {:headers {"Authorization" (str "token " token)
-                                      "Accept" "application/vnd.github.v3+json"}}) :body))))
+  (let [url (str "https://api.github.com/repos/" org "/" repo "/events?page=" page)
+        headers (if (empty? token) nil {"Authorization" (str "token " token)})]
+    (json/read-str ((http-client/get url {:headers headers}) :body))))
 
 (defn events
   ([org repo token page]